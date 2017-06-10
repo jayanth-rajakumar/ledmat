88be164386dff133a60a5eb948b1f43b549b8956
Jayanth R <jayanth.rajakumar@gmail.com>
2017-06-10 21:58:25 +0530

    Fully working version that doesn't skip every other packet

--- ledmat.ino
+++ ledmat.ino
@@ -11,109 +11,6 @@
 
 MD_Parola P = MD_Parola(CS_PIN, MAX_DEVICES);
 
-int serial_in = 2,pulse;
-long temp;
-volatile long duration=0,m1,m2;
-volatile int i=0,arr[90],toggle=0;
-String packet="",current_time="";
-
-void getbytes(String strbyte);
-int twopwr(int);
-
-//Comment
-void setup()
-{
- P.begin();
-  P.setIntensity(0);
-  P.setFont(xantofont);
-
- 
-  Serial.begin(9600);
-  pinMode(serial_in, INPUT);
-  digitalWrite(serial_in,HIGH);
-
-  
-}
-
-void loop() {
-
-  i=0,toggle=0;
-  for(int k=0;k<90;k++)
-  arr[k]=0;
-
-   while(1)
-   
-  {
-    temp=pulseIn(serial_in, LOW);
-    if(temp>56000 and temp<66000)
-    break;
-  }
-   
-  attachInterrupt(digitalPinToInterrupt(serial_in), isr, CHANGE);
-  while(toggle==0);
-  detachInterrupt(digitalPinToInterrupt(serial_in));
-
-  pulse=1;packet="";current_time="";
-  for(int k=1;arr[k]!=0;k++)
-  {
-    for(int j=0;j<round((float)arr[k]/828);j++)
-    {
-      packet+=String(!pulse);
-    }
-    pulse=!pulse;
-  }
-
-  for(int k=1;k<=5;k++)
- getbytes(packet.substring(k*10+1,(k+1)*10-1));
- current_time=current_time.substring(0,1)+':'+current_time.substring(1,3)+'.'+current_time.substring(3,5);
-  Serial.println(current_time);
-  
-  char charbuf[10];
-  current_time.toCharArray(charbuf,10);
-  P.displayText(charbuf, PA_RIGHT, 0, 0, PA_PRINT, PA_NO_EFFECT);
-  P.displayAnimate();
-}
-
-void isr()
-{
-  m1=micros();
-  if((m1-m2)>56000 and (m1-m2)<66000)
-  {toggle=1;
-  return;
-  }
-arr[i]=m1-m2;
-i++;
-m2=micros();
-}
-
-void getbytes(String strbyte)
-{
-
-  int digit=0;
-  for(int k=0;k<8;k++)
-  {
-   if(strbyte[k]=='1')
-   {
-    digit+=twopwr(k);
-      
-   }
-  }
-
-  char c=digit;
- current_time+=String(c);
-  
-}
-
-//Returns 2 raised to the specified integer. Required because pow() from math.h has rounding errors.
-int twopwr(int a)
-{
-  int out=1;
-  for(int l=0;l<a;l++)
-  out=out*2;
-
-  return out;
-}
-
 MD_MAX72XX::fontType_t xantofont[] PROGMEM = {
   0,    // 0 - 'Empty Cell'
   5, 62, 91, 79, 91, 62,    // 1 - 'Sad Smiley'
@@ -240,3 +137,140 @@ MD_MAX72XX::fontType_t xantofont[] PROGMEM = {
   5, 68, 100, 84, 76, 68,   // 122 - 'z'
 
 };
+
+int serial_in = 2,pulse;
+long temp;
+bool begin_=1;
+volatile long duration=0,m1,m2;
+volatile int i=0,arr[90],toggle=0;
+String packet="",current_time="";
+
+void getbytes(String strbyte);
+int twopwr(int);
+
+
+
+void setup()
+{
+ P.begin();
+  P.setIntensity(0);
+  P.setFont(xantofont);
+
+ 
+  Serial.begin(115200);
+  pinMode(serial_in, INPUT);
+  digitalWrite(serial_in,HIGH);
+
+     while(1)
+   
+  {
+    temp=pulseIn(serial_in, LOW);
+    if(temp>56000 and temp<66000)
+    break;
+  }
+  
+}
+
+void loop() {
+
+  i=0,toggle=0;
+  for(int k=0;k<90;k++)
+  arr[k]=0;
+
+
+   
+  attachInterrupt(digitalPinToInterrupt(serial_in), isr, CHANGE);
+  while(toggle==0);
+  detachInterrupt(digitalPinToInterrupt(serial_in));
+  
+
+
+  for(int k=0;k<90;k++)
+  {
+    Serial.print(String(arr[k])+",");
+  }
+
+
+  
+  pulse=1;packet="";current_time="";
+  for(int k=1;arr[k]!=0;k++)
+  {
+    for(int j=0;j<round((float)arr[k]/828);j++)
+    {
+      packet+=String(!pulse);
+    }
+    pulse=!pulse;
+  }
+  
+  
+    
+  for(int k=1;k<=5;k++)
+ getbytes(packet.substring(k*10+1,(k+1)*10-1));
+ current_time=current_time.substring(0,1)+':'+current_time.substring(1,3)+'.'+current_time.substring(3,5);
+   
+  
+  char charbuf[10];
+  current_time.toCharArray(charbuf,10);
+  
+  P.displayText(charbuf, PA_RIGHT, 0, 0, PA_PRINT, PA_NO_EFFECT);
+ P.displayAnimate();
+  while(digitalRead(serial_in)==0);
+}
+
+void isr()
+{
+  m1=micros();
+
+ if(m1-m2>58000 and begin_==true)
+  {
+    Serial.println("Start");begin_=false;
+    
+  }
+  else
+  {
+     arr[i]=m1-m2;
+    if(i>5 and round((float)arr[i]/750)==4 and round((float)arr[i-1]/750)==2 and round((float)arr[i-2]/750)==1 and round((float)arr[i-3]/750)==1 and round((float)arr[i-4]/750)==1 and round((float)arr[i-5]/750)==1)
+   {
+    
+    Serial.println();
+    m2=micros();
+    toggle=1;
+    return;
+   }
+   
+   // Serial.print(String(arr[i])+"-");
+    i++;
+  }
+  m2=micros();
+
+}
+
+void getbytes(String strbyte)
+{
+
+  int digit=0;
+  for(int k=0;k<8;k++)
+  {
+   if(strbyte[k]=='1')
+   {
+    digit+=twopwr(k);
+      
+   }
+  }
+
+  char c=digit;
+ current_time+=String(c);
+  
+}
+
+//Returns 2 raised to the specified integer. Required because pow() from math.h has rounding errors.
+int twopwr(int a)
+{
+  int out=1;
+  for(int l=0;l<a;l++)
+  out=out*2;
+
+  return out;
+}
+
+
