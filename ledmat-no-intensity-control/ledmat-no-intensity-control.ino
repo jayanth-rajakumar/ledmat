b460ff08c08eb46ed7cdcb8bb76840d5d9910bf0
Jayanth R <jayanth.rajakumar@gmail.com>
2017-07-28 17:49:33 +0530

    Updated no-intensity control

--- ledmat-no-intensity-control/ledmat-no-intensity-control.ino
+++ ledmat-no-intensity-control/ledmat-no-intensity-control.ino
@@ -285,6 +285,7 @@ int twopwr(int);
 void setup()
 {
  P.begin();
+ //Change 0 to any 0-15 integer to fix brightness
   P.setIntensity(0);
   P.setFont(xantofont);
 
@@ -309,7 +310,8 @@ void loop() {
   for(int k=0;k<90;k++)
   arr[k]=0;
   long test_=millis();
-  
+
+  /*
   attachInterrupt(digitalPinToInterrupt(serial_in), isr, CHANGE);
   while(toggle==0)
   {
@@ -320,13 +322,38 @@ void loop() {
     }
   }
   detachInterrupt(digitalPinToInterrupt(serial_in));
-  /*
+  */
+
+  delayMicroseconds(416);
+  
+            
+            for(int lx=0;lx<30;lx++)
+            {
+              arr[lx]=digitalRead(serial_in);    
+              delayMicroseconds(810);
+            }
+            
+            arr[30]=digitalRead(serial_in);           
+            delayMicroseconds(810);
+            
+            for(int lx=31;lx<90;lx++)
+            {
+              arr[lx]=digitalRead(serial_in);    
+              delayMicroseconds(810);
+            }
+            
+            
+            
+            
   for(int k=0;k<90;k++)
   {
-    Serial.print(String(arr[k])+",");
+   Serial.print(String(arr[k]));
   }
-  */ 
+  
+  
+  Serial.print("\n");
   pulse=1;packet="";current_time="";
+  /*
   for(int k=1;arr[k]!=0;k++)
   {
     for(int j=0;j<round((float)arr[k]/828);j++)
@@ -335,7 +362,17 @@ void loop() {
     }
     pulse=!pulse;
   }
-    
+  */
+  packet="";
+  for(int k=0;k<90;k++)
+  {
+    if(arr[k]==0){
+      packet+="1";
+    }
+    else
+    packet+="0";
+    }
+  //  Serial.println(packet);
  for(int k=1;k<=5;k++)
  getbytes(packet.substring(k*10+1,(k+1)*10-1));
 
@@ -344,55 +381,28 @@ void loop() {
   char charbuf[10];
   current_time.toCharArray(charbuf,10);
 
-  if(isDigit(charbuf[0]) and isDigit(charbuf[6]))
+  if(isDigit(charbuf[0]) and isDigit(charbuf[6]) and isDigit(charbuf[2]) and isDigit(charbuf[3]) and isDigit(charbuf[5]))
   {
     P.displayText(charbuf, PA_LEFT, 0, 0, PA_PRINT, PA_SCROLL_UP);
     P.displayAnimate(); 
   }
   else
   {
-   //Temporary hack to fill in skipped bits on G4 Pro timer. TODO: Implement packet detection alg from scratch using staggered sampling and remove this section.
-   
-    packet_=packet.substring(0,18) + "0" + packet.substring(18,38) + "1" + packet.substring(38,48)+"0"+ packet.substring(48);
+   //Hack to resolve last seconds on QJ Timer. TODO: Improve timing of digitalRead to fix this
 
-    current_time="";
-   
-    for(int k=1;k<=5;k++)
-    getbytes(packet_.substring(k*10+1,(k+1)*10-1));
+   charbuf[5]=qj_fix(charbuf[5]);
+   charbuf[6]=qj_fix(charbuf[6]);
 
-    current_time=current_time.substring(0,1)+':'+current_time.substring(1,3)+'.'+current_time.substring(3,5);
+        if(isDigit(charbuf[0]) and isDigit(charbuf[6]) and isDigit(charbuf[2]) and isDigit(charbuf[3]) and isDigit(charbuf[5]))
+        {
+          P.displayText(charbuf, PA_LEFT, 0, 0, PA_PRINT, PA_SCROLL_UP);
+          P.displayAnimate(); 
+        }
     
-    char charbuf2[10];  
-    current_time.toCharArray(charbuf2,10);
-   
-    if(charbuf2[6]=='`')
-      charbuf2[6]='0';
-   
-    if(isDigit(charbuf2[0]) and isDigit(charbuf2[6]))
-    {
-       P.displayText(charbuf2, PA_LEFT, 0, 0, PA_PRINT, PA_SCROLL_UP);
-       P.displayAnimate();
-    }
    
   }
 
-  //Read pot and adjust brightness
-  {
-      int pot=analogRead(A0);   
-      if(pot<100)
-         P.setIntensity(0);
-      else if(pot>150 and pot<250)
-         P.setIntensity(3);
-      else if(pot>300 and pot<400)
-         P.setIntensity(6);
-      else if(pot>450 and pot<550)
-         P.setIntensity(9);
-      else if(pot>600 and pot<700)
-         P.setIntensity(12);
-      else if(pot>750 and pot<850)
-         P.setIntensity(15);
-    
-  }
+ 
   long test=millis();
   while(digitalRead(serial_in)==0)
   {
@@ -461,4 +471,51 @@ int twopwr(int a)
   return out;
 }
 
+char qj_fix(char ch)
+{
+  switch(ch)
+  {
+    case '`':
+    return '0';
+    break;
+
+    case 'b':
+    return '1';
+    break;
+
+    case 'd':
+    return '2';
+    break;
+
+    case 'f':
+    return '3';
+    break;
+
+    case 'h':
+    return '4';
+    break;
+
+    case 'j':
+    return '5';
+    break;
+
+    case 'l':
+    return '6';
+    break;
+
+    case 'n':
+    return '7';
+    break;
+
+    case 'p':
+    return '8';
+    break;
+
+    case 'r':
+    return '9';
+    break;
+
+    
+  }
+}
 
